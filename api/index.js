124e19e0b93e4319f97328ec0bdf22d6a809ba27
MustafaTahirAkdemir <themtahir@gmail.com>
2024-09-23 04:38:42 +0300

    create signin api route

--- api/index.js
+++ api/index.js
@@ -24,6 +24,9 @@ app.listen(3131, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+
+
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
