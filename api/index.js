02ec4370c7a5a0d0308a5b6b3777166bc6440a0d
MustafaTahirAkdemir <themtahir@gmail.com>
2024-09-24 05:40:29 +0300

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -25,6 +26,8 @@ app.listen(3131, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+
 
 
 
